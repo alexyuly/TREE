39b5ef9a7966c071ce2531c20d3868d629078722
Alex Yuly <alexyuly2@gmail.com>
2019-09-14 14:22:04 -0700

    Read lines one at a time

--- compiler/main.js
+++ compiler/main.js
@@ -1,1 +1,14 @@
-// TODO
+#!/usr/bin/env node
+
+const fs = require('fs');
+const readline = require('readline');
+
+const [, , sourcePath] = process.argv;
+
+const sourceInterface = readline.createInterface({
+  input: fs.createReadStream(sourcePath)
+});
+
+sourceInterface.on('line', line => {
+  console.log(line);
+});
